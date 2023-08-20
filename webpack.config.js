31eccbe3d24e37a96261886dcbc6cd5703ee45b5
VSalamakha <salamakhaviktor@gmail.com>
2023-08-20 14:55:40 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
-    publicPath: "/hillel-hometask-28th/",
+    publicPath: "/",
   },
   module: {
     rules: [
